24f61a61f220843075792aaf8556d3eb716a6a2c
JACLYNEE <410631757@gms.tku.edu.tw>
2024-07-19 01:59:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, jsonify, render_template, request,redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 
-app= Flask(__name__, static_url_path='/static', template_folder="templates")#_name_ 代表目前執行的模組
+app = Flask(__name__, static_url_path='/static', template_folder="templates")
+app.secret_key = 'your_secret_key'
 
-@app.route("/")#函式的裝飾(Decorator): 已函式為基礎，提供附加的功能
+
+@app.route("/")
 def index():
-    return render_template('/index.html')
-    #return "This is home"
+    return render_template('index.html')
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
+
+@app.route("/login-page", methods=['GET', 'POST'])
+def login_page():
     if request.method == 'POST':
         user_name = request.form.get('user_name')
         password = request.form.get('password')
@@ -17,42 +19,55 @@ def login():
             return redirect(url_for('main_page'))
         else:
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('login_page'))
     return render_template('login-page.html')
-    
+
+
 @app.route("/main-page")
 def main_page():
-    return render_template('/main-page.html')
-    #return "this is main page"
+    return render_template('main-page.html')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        user_name= request.form.get('user_name')
+        user_name = request.form.get('user_name')
         email = request.form.get('email')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
 
         if password == confirm_password:
             return redirect(url_for('/'))
-    return render_template('/register.html')
-    #return "this is register"
+    return render_template('register.html')
 
-@app.route('/chat_room', methods=['GET'])
+
+@app.route('/chat-room', methods=['GET', 'POST'])
 def chat_room():
-    return 'this is chat room'
+    return render_template('chat-room.html')
 
-@app.route('/MBTIclassification', methods=['GET'])
-def MBTIclassification():
-    return 'this is MBTI classification'
 
-@app.route('/Profile', methods=['GET'])
-def Profile():
-    return 'this is Profile'
+@app.route('/m-b-t-i-classification', methods=['GET'])
+def mbticlassification():
+    return render_template('m-b-t-i-classification.html')
+
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    return render_template('profile.html')
+
 
 @app.route('/setting', methods=['GET'])
 def setting():
-    return 'this is setting'
+    return render_template('setting.html')
+
+
+@app.route('/sentiment-Analysis', methods=['GET'])
+def sentiment_analysis():
+    return render_template('sentiment-Analysis.html')
+
+
+if __name__ == "__main__":
+    app.run()
 
 @app.route('/Sentiment_Analysis', methods=['GET'])
 def Sentiment_Analysis():
