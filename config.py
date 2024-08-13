b75b75f71f5fd910140e0d04d6c7d8f0bf6b236a
74Julian74 <130432785+74Julian74@users.noreply.github.com>
2024-08-13 21:57:32 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-#這個檔案負責應用程式的配置，主要是設置一些基本的配置參數。
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
